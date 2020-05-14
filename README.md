fce21fff7537360c5a9bfcccda23326c005c08ff
bj <bj@126.com>
2020-05-14 18:32:26 +0800

    modify files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 011-git-demo
 welcome git
+welcome github
--- src/main/java/com/wkcto/maven/Demo.java
+++ src/main/java/com/wkcto/maven/Demo.java
@@ -3,5 +3,6 @@ package com.wkcto.maven;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Github");
     }
 }
